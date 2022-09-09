3edc3221aa95b1c71559edcc8c8c9cb8ce48f882
Groupe SmarTech <smartech.groupe@gmail.com>
2022-09-09 17:07:51 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        police: "#244c93",
+        pompier: "#dc1113",
+        samu: "#e9e61e",
+      },
+    },
+  },
+  plugins: [],
+}
